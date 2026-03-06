27b6320a90d38d764915fe156130d148b7ce0487
werdna <werdnadev@proton.me>
2026-03-06 10:44:49 +0200

    header fix

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,19 +13,21 @@ function RootComponent() {
 	return (
 		<React.Fragment>
 			<main className="h-full flex flex-col">
-				<header className="h-[10%] z-100 fixed top-0 left-0 w-full flex items-center justify-between gap-4 p-4 md:p-8 max-h-[200px] element shadow-lg">
-					<h1 className="font-[700] text-xl md:text-2xl">
-						Where in the world?
-					</h1>
+				<header className="h-[10%] z-100 fixed top-0 left-0 w-full  p-4 md:p-8 max-h-[200px] element shadow-lg">
+					<div className="h-full w-full flex items-center justify-between gap-4 max-w-7xl mx-auto">
+						<h1 className="font-[700] text-xl md:text-2xl">
+							Where in the world?
+						</h1>
 
-					<button
-						type="button"
-						onClick={toggleTheme}
-						className="flex text-sm font-[600] items-center gap-1"
-					>
-						<Icon theme={theme} />
-						<p>{theme} Mode</p>
-					</button>
+						<button
+							type="button"
+							onClick={toggleTheme}
+							className="flex text-sm font-[600] items-center gap-1"
+						>
+							<Icon theme={theme} />
+							<p>{theme} Mode</p>
+						</button>
+					</div>
 				</header>
 				<section className="mt-20 flex-1 py-12">
 					<Outlet />
